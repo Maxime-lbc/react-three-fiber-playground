b377d7522bafa6fba440551b5b6fdbd7d2c8493c
Maxime LE BONNIEC <maxime.lbc.pro@gmail.com>
2021-03-25 20:39:27 +0100

    Add lights

--- src/App.js
+++ src/App.js
@@ -11,7 +11,24 @@ function App() {
     <>
       <Canvas colorManagement>
 
+
         <ambientLight intensity={0.3} />
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+
+        
 
         <BoxForm width={1} height={1} depth={1} position={[-2, 1 ,1]} color="blue" />
         <BoxForm width={1} height={1} depth={1} position={[-1, -1, -1]} color="black" />
