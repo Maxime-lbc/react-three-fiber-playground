6a3680d772f0c375fc57faf9eadae9e9a9353330
Maxime LE BONNIEC <maxime.lbc.pro@gmail.com>
2021-03-25 20:25:24 +0100

    Add OrbitControl

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "react-three-fiber";
 import BoxForm from "./styles/components/forms/BoxForm";
 
@@ -15,6 +16,8 @@ function App() {
         <BoxForm width={1} height={1} depth={1} position={[-2, 1 ,1]} color="blue" />
         <BoxForm width={1} height={1} depth={1} position={[-1, -1, -1]} color="black" />
         <BoxForm width={1} height={1} depth={1} position={[1, 1 ,1]} color="white" />
+
+        <OrbitControls />
       </Canvas>
     </>
   );
